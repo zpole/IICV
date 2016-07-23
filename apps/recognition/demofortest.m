64b005a4a1977a529ac88f556d2cb74007846815
zpole <zhi951206@gmail.com>
2016-07-23 19:18:57 +0800

    first

---
+++ apps/recognition/demofortest.m
@@ -0,0 +1,16 @@
+function demofortest()
+
+im = imread(fullfile(vl_root,'data','roofs1.jpg')) ;
+im = im2single(im) ;
+%im = im(1:128,end-128+1:end,:) ;
+  %slic
+  segments = vl_slic(im, 500, 1, 'verbose') ;
+  [sx,sy]=vl_grad(double(segments), 'type', 'forward') ;
+  s = find(sx | sy) ;     %save the number of edge pixels
+  %slic end
+  
+  h = size(im,1) ;
+  features = getDenseSIFT(im) ;     %getdensesift（default）
+  fprintf('%d,%d\n',size(s,1),size(features.frame,2));
+  feature=filtrate_sift_by_slic(h,s,features);       %filtrate sift descriptor by slic edge
+save testme.mat;
---
+++ apps/recognition/filtrate_sift_by_slic.m
@@ -0,0 +1,54 @@
+function feature=filtrate_sift_by_slic(hight,s,features)
+%filtrate sift descriptor by slic edge. 
+%%wight is the wight of image
+%hight is the hight of image
+%s is the matrix include the edge of slic
+%features is the sift features
+
+for i = 1:size(s,1)
+    temp(1,i) = mod(s(i,1) , hight);
+    temp(2,i) = fix(s(i,1) / hight) + 1;
+end
+
+% Buld a kd-tree
+kdtree = vl_kdtreebuild( temp ) ;
+
+Q = [features.frame(1,1);features.frame(2,1)];
+[index, dist] = vl_kdtreequery (kdtree, temp, Q, 'MaxNumComparisons', 1);
+if dist <= features.frame(3,1);
+    features.frame(:,1) = 0;
+    features.descr(:,1) = 0;
+    features.contrast(:,1) = 0;
+end
+
+for i = 2:size(features.frame,2)    
+    
+    a = 8 / 3 * abs(temp(1,index) - features.frame(1,i));
+    a = a * a;
+    b = 8 / 3 * abs(temp(2,index) - features.frame(2,i));
+    b = b * b;
+    c = features.frame(3,i) * features.frame(3,i);
+    if a + b <= c
+        features.frame(:,i) = 0;
+        features.descr(:,i) = 0;
+        features.contrast(:,i) = 0;
+        continue
+        
+    else
+        Q = [features.frame(1,i);features.frame(2,i)];
+        [index, dist] = vl_kdtreequery (kdtree, temp, Q, 'MaxNumComparisons', 1);
+        if dist <= features.frame(3,i);
+            features.frame(:,i) = 0;
+            features.descr(:,i) = 0;
+            features.contrast(:,i) = 0;
+        end
+    end
+    
+end
+
+features.frame(:,all(features.frame==0,1))=[] ;
+features.descr(:,all(features.descr==0,1))=[] ;
+features.contrast(:,all(features.contrast==0,1))=[] ;
+
+save aha.mat;
+feature = features;
